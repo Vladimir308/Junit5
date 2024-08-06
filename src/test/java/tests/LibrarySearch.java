7c124c72185ede81f6e30b2ebea2de7ea09be5ac
Vladimir Milyukov <vova.milyukov.94@bk.ru>
2024-08-07 01:01:32 +0300

    done

--- src/test/java/tests/LibrarySearch.java
+++ src/test/java/tests/LibrarySearch.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 import org.junit.jupiter.params.provider.ValueSource;
 import static com.codeborne.selenide.Condition.text;
@@ -10,7 +11,7 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 
-public class ParameterizedTest {
+public final class LibrarySearch {
     @BeforeEach
     void setUp() {
         open("https://www.labirint.ru/");
@@ -19,16 +20,16 @@ public class ParameterizedTest {
     @ValueSource(strings = {
             "Книга билла", "Книга для записей",
     })
-    @org.junit.jupiter.params.ParameterizedTest(name = "Для поискового запроса {0} должен отдавать не пустой список табличек")
+    @ParameterizedTest(name = "Для поискового запроса {0} должен отдавать не пустой список табличек")
     @Tag("BLOCKER")
-    void searchResultsShouldNotBeEmpty(String searchQuery) {
+    void searchResultsShouldNotBeEmptyTest(String searchQuery) {
         $("#search-field").setValue(searchQuery).pressEnter();
         $(".search-tab").shouldBe(visible);
     }
     @CsvFileSource(resources = "/test_data/testWithCsvFileSource.csv")
-    @org.junit.jupiter.params.ParameterizedTest(name = "Для поискового запроса {0} должен быть автор")
+    @ParameterizedTest(name = "Для поискового запроса {0} должен быть автор")
     @Tag("BLOCKER")
-    void testWithCsvFileSource(String searchQuery, String expectedLink) {
+    void testWithCsvFileSourceTest(String searchQuery, String expectedLink) {
         $("#search-field").setValue(searchQuery).pressEnter();
         $(".search-tab")
                 .shouldHave(text(expectedLink));
--- src/test/java/tests/YandexPhone.java
+++ src/test/java/tests/YandexPhone.java
@@ -3,21 +3,24 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
-public class ValueSource {
+public final class YandexPhone {
     @BeforeEach
     void setUp() {
         open("https://market.yandex.ru/");
         Configuration.pageLoadStrategy = "eager";
     }
-    @org.junit.jupiter.params.provider.ValueSource(strings = {
+    @ValueSource(strings = {
             "iphone 15", "samsung s24",
     })
-    @org.junit.jupiter.params.ParameterizedTest(name = "Для поискового запроса {0} должен отдавать не пустой список табличек")
+    @ParameterizedTest(name = "Для поискового запроса {0} должен отдавать не пустой список табличек")
     @Tag("BLOCKER")
-    void searchResultsShouldNotBeEmpty(String searchQuery) {
+    void searchResultsShouldNotBeEmptyTest(String searchQuery) {
         $("#header-search").setValue(searchQuery).pressEnter();
         $("#SerpStatic").shouldBe(visible);
     }
